1a7ca90cc63d38c7ab704fc9a0ee5d98c180b8b6
Escert <dragon.ngc@gmx.de>
2022-06-26 15:37:32 +0200

    Edit README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# sudoku
+# How to build
+
+`mvn clean install`
+
+# How to run
+
+Build a version using Maven and then either directly execute the created jar or run
+`java -jar Sudoku.jar`
--- Sudoko/src/main/java/com/github/escert/sudoku/SudokuApplication.java
+++ Sudoko/src/main/java/com/github/escert/sudoku/SudokuApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class SudokuApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
+		SpringApplication.run(SudokuApplication.class, args);
 	}
 }
